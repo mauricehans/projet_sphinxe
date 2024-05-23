42e9ec9b474d4de4c7609e9bea28b02b048ad719
Jason-24 <135524962+Jason-Tch24@users.noreply.github.com>
2024-05-23 16:58:30 +0200

    Ajout de la generation

--- suite_de_lettre.py
+++ suite_de_lettre.py
@@ -1,17 +1,11 @@
 import random
 from collections import defaultdict
-
-lettres = ['a', 'à', 'â', 'æ', 'b', 'c', 'ç', 'd', 'e', 'é', 'è', 'ê', 'ë', 'f', 'g', 'h', 'i', 'î', 'ï', 'j', 'k', 'l', 'm', 'n', 'o', 'ô', 'œ', 'p', 'q', 'r', 's', 't', 'u', 'ù', 'û', 'ü', 'v', 'w', 'x', 'y', 'ÿ', 'z']
-
 # Créer une matrice pour stocker les occurrences
-lettre = {}
+
 occurrence = defaultdict(lambda: defaultdict(int))
 
 #[[0 for _ in range(len(lettres))] for _ in range(len(lettres))]
 # Fonction pour vider un tableau
-def vide(T):
-    T.clear()
-    return T
 
 # Lire les mots depuis le fichier
 with open('liste.de.mots.francais.frgut.txt', 'r', errors='ignore') as m:
@@ -22,10 +16,28 @@ with open('liste.de.mots.francais.frgut.txt', 'r', errors='ignore') as m:
             index_j = mot[i + 1]
             occurrence[index_i][index_j] += 1
 
-# Afficher la matrice des occurrences
+# Afficher la matrice des probabiliter
 
-print(occurrence)
 for lettre1, suivie in occurrence.items():
+    total_occurence = sum(suivie.values())
     for lettre2, count in suivie.items():
-        print(f"{lettre1} -> {lettre2}: {count}")
-       
+        occurrence[lettre1][lettre2] = count/ total_occurence
+        #print(f"{lettre1} -> {lettre2}: {count/total_occurence}")
+for j in range(0,10):
+    premier_lettre = random.choice(list(occurrence.keys()))
+    mot = [premier_lettre]
+    probabiliter = 0
+    for i in range (0,10,1):
+        if probabiliter >= 1 :
+            break
+        else:
+            liste_des_letter_probable = occurrence.get(premier_lettre, {})
+            if not liste_des_letter_probable:
+                break
+            liste_lettre_suivie = list(liste_des_letter_probable.keys())
+            liste_proba_suivie = list(liste_des_letter_probable.values())
+            #choix d'une lettre
+            nouvelle_lettre = random.choices(liste_lettre_suivie, liste_proba_suivie)[0]
+            probabiliter += occurrence[premier_lettre][nouvelle_lettre]
+            mot.append(nouvelle_lettre)
+    print(''.join(mot))
